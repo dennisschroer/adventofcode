85957930b7c6cc987950635a7c9d786dd6fabddc
Dennis Schroer <dennis.schroer@topicus.nl>
2020-12-01 22:21:43 +0100

    Day 1

--- 2020/01/01.hs
+++ 2020/01/01.hs
@@ -4,11 +4,17 @@ import Data.List
 pairs :: [a] -> [(a, a)]
 pairs l = [(x,y) | (x:ys) <- tails l, y <- ys]
 
+combinations k ns = filter ((k==).length) $ subsequences ns
+
 triplets :: [a] -> [(a, a, a)]
-triplets l = [(x,y,z) | (x:y:zs) <- tails l, z <- zs]
+-- triplets l = [(x,y,z) | (x:y:zs) <- tails l, z <- zs]
+triplets l = [(x,y,z) | [x,y,z] <- filter ((3==).length) $ subsequences l]
 
 pairSum :: (Int, Int) -> Int
 pairSum (x, y) =  x + y
+
+pairProduct :: (Int, Int) -> Int
+pairProduct (x, y) =  x * y
 
 sumIs2020 :: (Int, Int) -> Bool
 sumIs2020 (x, y) = pairSum (x,y) == 2020
@@ -16,22 +22,25 @@ sumIs2020 (x, y) = pairSum (x,y) == 2020
 tripleSum :: (Int, Int, Int) -> Int
 tripleSum (x, y, z) = x + y + z
 
+tripleProduct :: (Int, Int, Int) -> Int
+tripleProduct (x, y, z) = x * y * z
+
 sumIs2020Triple :: (Int, Int, Int) -> Bool
 sumIs2020Triple (x, y, z) = tripleSum (x, y, z) == 2020
-
-displayResult :: Maybe (Int, Int) -> String
-displayResult p = maybe "There was no result" (\(x,y) -> ("The result is " ++ (show (x*y)))) p
 
 main = do
   contents <- readFile "input"
   let numbers = sort $ map (read::String->Int) (lines contents)
   let pair = find sumIs2020 $ pairs numbers
 
+  print "part 1"
   print pair
-  print (displayResult pair)
+  print (maybe 0 pairProduct pair)
 
-  print $ show $ map tripleSum $ triplets numbers
+  -- print $ show $ map tripleSum $ triplets numbers
 
   let triplet = find sumIs2020Triple $ triplets numbers
 
+  print "part 2"
   print triplet
+  print (maybe 0 tripleProduct triplet)
