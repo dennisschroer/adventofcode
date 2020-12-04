55d2d37f8f78e48673f7251f5def73bd8ef74e1b
Dennis Schroer <dennis.schroer@topicus.nl>
2020-12-04 21:35:35 +0100

    Day 3 Part 2

--- 2020/03/03.hs
+++ 2020/03/03.hs
@@ -1,27 +1,26 @@
-
-
 getCharAt :: [[Char]] -> (Int, Int) -> Char
 getCharAt field (x,y) =
   let width = length $ field !! 0
   in field !! y !! (x `mod` width)
 
 calculateAmountOfTrees :: String -> Int -> Int -> Int
-calculateAmountOfTrees contents right down =
+calculateAmountOfTrees input right down =
   let
-    height = length $ lines contents
-    width = length $ head $ lines contents
+    height = length $ lines input
+    width = length $ head $ lines input
     steps = [0..((height `div` down) - 1)]
-  in length $ filter ('#'==) $ [ lines contents !! (i*down) !! (mod (i*right) width) | i <- steps]
+  in length $ filter ('#'==) $ [ lines input !! (i*down) !! (mod (i*right) width) | i <- steps]
 
 main = do
+  input <- readFile "input"
+
   -- Part 1, long version
-  contents <- readFile "input"
-  let field = lines contents
+  let field = lines input
 
   let step_down = 1
   let step_right = 3
-  let height = length $ lines contents
-  let width = length $ lines contents !! 0
+  let height = length $ lines input
+  let width = length $ lines input !! 0
 
   let steps = [0..((height `div` step_down) - 1)]
   let coordinates = [(i*step_right, i*step_down) | i <- steps]
@@ -34,22 +33,23 @@ main = do
   let width = length $ field !! 0
 
   -- Part 1, short version
-  contents <- readFile "input"
-  print $ length $ filter ('#'==) $ [ lines contents !! i !! (mod (i*3) $ length $ head $ lines contents) | i <- [0..((length $ lines contents) - 1)]]
+  print $ length $ filter ('#'==) $ [ lines input !! i !! (mod (i*3) $ length $ head $ lines input) | i <- [0..((length $ lines input) - 1)]]
 
   -- Part 1, with function
-  contents <- readFile "input"
-  print $ calculateAmountOfTrees contents 3 1
+  print $ calculateAmountOfTrees input 3 1
 
   -- Part 2
-  let slope_1_1 = calculateAmountOfTrees contents 1 1
-  let slope_3_1 = calculateAmountOfTrees contents 3 1
-  let slope_5_1 = calculateAmountOfTrees contents 5 1
-  let slope_7_1 = calculateAmountOfTrees contents 7 1
-  let slope_1_2 = calculateAmountOfTrees contents 1 2
+  let slope_1_1 = calculateAmountOfTrees input 1 1
+  let slope_3_1 = calculateAmountOfTrees input 3 1
+  let slope_5_1 = calculateAmountOfTrees input 5 1
+  let slope_7_1 = calculateAmountOfTrees input 7 1
+  let slope_1_2 = calculateAmountOfTrees input 1 2
   print slope_1_1
   print slope_3_1
   print slope_5_1
   print slope_7_1
   print slope_1_2
   print $ slope_1_1 * slope_3_1 * slope_5_1 * slope_7_1 * slope_1_2
+
+  -- Or, in one line
+  print $ foldl (*) 1 $ map (\(x,y) -> calculateAmountOfTrees input x y) [(1,1), (3,1), (5,1), (7,1), (1,2)]
