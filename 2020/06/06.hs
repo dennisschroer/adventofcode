e41e6b05abe686b29938ea2288ffb8a4e1b6eb8a
Dennis Schroer <dennis.schroer@topicus.nl>
2020-12-06 13:00:11 +0100

    Day 6

---
+++ 2020/06/06.hs
@@ -0,0 +1,26 @@
+import Data.List
+import Data.List.Split
+
+main = do
+  test <- readFile "test"
+  input <- readFile "input"
+
+  putStrLn "== Test for part 1 =="
+  print $ map (combinedAnswersUnion . lines) $ splitOn "\n\n" test
+  print $ sum $ map (length . combinedAnswersUnion . lines) $ splitOn "\n\n" test
+
+  putStrLn "== Test for part 2 =="
+  print $ map (combinedAnswersIntersection . lines) $ splitOn "\n\n" test
+  print $ sum $ map (length . combinedAnswersIntersection . lines) $ splitOn "\n\n" test
+
+  putStrLn "== Part 1 =="
+  print $ sum $ map (length . combinedAnswersUnion . lines) $ splitOn "\n\n" input
+
+  putStrLn "== Part 2 =="
+  print $ sum $ map (length . combinedAnswersIntersection . lines) $ splitOn "\n\n" input
+
+combinedAnswersUnion :: [String] -> String
+combinedAnswersUnion answers = foldl union "" answers
+
+combinedAnswersIntersection :: [String] -> String
+combinedAnswersIntersection answers = foldl intersect ['a'..'z'] answers
