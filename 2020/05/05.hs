5c240f0c8b807ac0e251873c0ee33cee08d421a5
Dennis Schroer <dennis.schroer@topicus.nl>
2020-12-06 13:22:24 +0100

    Day 5 oneliners

--- 2020/05/05.hs
+++ 2020/05/05.hs
@@ -17,15 +17,27 @@ main = do
 
   putStrLn "== Part 1 =="
   print $ maximum $ map seatId $ lines input
+  print $ maximum $ map seatId2 $ lines input
+
+  -- Or, in one line:
+  print $ maximum $ map (sum . map (2^) . findIndices (\c -> elem c "BR") . reverse) $ lines input
 
   putStrLn "== Part 2 =="
   print $ [54..930] \\ (map seatId $ lines input)
+  print $ [54..930] \\ (map seatId2 $ lines input)
+
+  -- Or, in one line:
+  print $ [54..930] \\ (map (sum . map (2^) . findIndices (\c -> elem c "BR") . reverse) $ lines input)
 
 codeToRow :: String -> Int
-codeToRow code = sum $ map (2^) $ findIndices (=='B') $ reverse $ take 7 code
+codeToRow code = sum $ map (2^) $ elemIndices 'B' $ reverse $ take 7 code
 
 codeToSeat :: String -> Int
-codeToSeat code = sum $ map (2^) $ findIndices (=='R') $ reverse $ drop 7 code
+codeToSeat code = sum $ map (2^) $ elemIndices 'R' $ reverse $ drop 7 code
 
 seatId :: String -> Int
 seatId code = (codeToRow code) * 8 + (codeToSeat code)
+
+-- No need to split the code first: the entire code is one binary number, with B and R as 1:
+seatId2 :: String -> Int
+seatId2 code = sum $ map (2^) $ findIndices (\c -> elem c "BR") $ reverse code
