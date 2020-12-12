6643ed22511936bf891460c306222d6db970a7c9
Dennis Schroer <dennis.schroer@topicus.nl>
2020-12-12 16:07:28 +0100

    Day 12 part 2 WIP

--- 2020/12/12.hs
+++ 2020/12/12.hs
@@ -4,6 +4,7 @@ import           Data.Maybe
 
 main = do
   test <- readFile "test"
+  test_extended <- readFile "test_extended"
   input <- readFile "input"
 
   putStrLn "== Test Part 1 =="
@@ -12,11 +13,24 @@ main = do
 
   putStrLn "== Part 1 =="
   print $ navigate (0, 0, 90) $ lines input
-  print $ (\(x,y,_) -> manhattan x y ) $ navigate (0, 0, 90) $ lines input
+  print $ (\(x,y,_) -> manhattan x y) $ navigate (0, 0, 90) $ lines input
 
   putStrLn "== Test Part 2 =="
+  -- Output: (214,-72,4,-10)
+  print $ navigateWaypoint (0, 0, 10, 1) $ lines test
+  -- Output: 286
+  print $ (\(x,y,_,_) -> manhattan x y) $ navigateWaypoint (0, 0, 10, 1) $ lines test
+
+  -- Output: (214,-72,4,-10)
+  print $ navigateWaypoint (0, 0, 10, 1) $ lines test_extended
+  -- Output: 286
+  print $ (\(x,y,_,_) -> manhattan x y) $ navigateWaypoint (0, 0, 10, 1) $ lines test_extended
 
   putStrLn "== Part 2 =="
+  -- Output: (-2470,8091,-67,9)
+  print $ navigateWaypoint (0, 0, 10, 1) $ lines input
+  -- Output: 10561
+  print $ (\(x,y,_,_) -> manhattan x y) $ navigateWaypoint (0, 0, 10, 1) $ lines input
 
 
 navigate :: (Int, Int, Int) -> [String] -> (Int, Int, Int)
@@ -37,3 +51,21 @@ navigate (x, y, direction) (command:commands) =
 
 manhattan :: Int -> Int -> Int
 manhattan x y = (abs x)+(abs y)
+
+navigateWaypoint :: (Int, Int, Int, Int) -> [String] -> (Int, Int, Int, Int)
+-- Base case: no commands left
+navigateWaypoint (x, y, wx, wy) [] = (x, y, wx, wy)
+-- Recursive case: at lease one command left
+-- x, y = ships current position. Positive=east/north
+-- wx, xy = Position of waypoint relative to ship. Positive=east/north
+navigateWaypoint (x, y, wx, wy) (command:commands) =
+  let action = head command
+      value  = read $ tail command
+  in case action of
+    'N' -> navigateWaypoint (x, y, wx, wy+value) commands
+    'S' -> navigateWaypoint (x, y, wx, wy-value) commands
+    'E' -> navigateWaypoint (x, y, wx+value, wy) commands
+    'W' -> navigateWaypoint (x, y, wx-value, wy) commands
+    'L' -> navigateWaypoint (x, y, -wy, wx) commands
+    'R' -> navigateWaypoint (x, y, wy, -wx) commands
+    'F' -> navigateWaypoint (x + value * wx, y + value * wy, wx, wy) commands
