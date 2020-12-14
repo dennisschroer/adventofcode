e225bf6f831c20304c266e254a3a33f50f48f816
Dennis Schroer <dennis.schroer@topicus.nl>
2020-12-15 00:26:25 +0100

    Day 14 part 2

--- 2020/14/14.hs
+++ 2020/14/14.hs
@@ -15,8 +15,12 @@ main = do
   print $  Map.foldr (+) 0 $ fst $ runProgram (Map.empty ,"") $ lines input
 
   putStrLn "== Test Part 2 =="
+  print $ expandAddress "000000000000000000000000000000X1001X" 42
+  print $ writeMemory2 (Map.empty, "000000000000000000000000000000X1001X") 42 100
+  print $ writeMemory2 (Map.empty, "00000000000000000000000000000000X0XX") 26 1  
 
   putStrLn "== Part 2 =="
+  print $ Map.foldr (+) 0 $ fst $ runProgram2 (Map.empty ,"") $ lines input
 
 runProgram :: (Map.Map Int Int, String) -> [String] -> (Map.Map Int Int, String)
 runProgram state [] = state
@@ -44,3 +48,31 @@ maskValue mask value =
     -- Zeros mask is used with bitwise and to force all zeroes to be zero in the output
     zerosMask = sum $ map (2^) $ elemIndices '1' $ replace "X" "1" $ reverse mask
     in (value .&. zerosMask) .|. onesMask
+
+runProgram2 :: (Map.Map Int Int, String) -> [String] -> (Map.Map Int Int, String)
+runProgram2 state [] = state
+runProgram2 (memory, mask) (instruction:instructions) =
+  let
+    parts = splitOn " = " instruction
+    command = head parts
+    argument = last parts
+    in case command of
+      "mask" -> runProgram2 (changeMask (memory, mask) argument) instructions
+      otherwise -> runProgram2 (writeMemory2 (memory, mask) (read $ init $ drop 4 command) (read argument)) instructions
+
+writeMemory2 :: (Map.Map Int Int, String) -> Int -> Int -> (Map.Map Int Int, String)
+writeMemory2 (memory, mask) address value = (foldr (\a -> Map.insert a value) memory (expandAddress mask address), mask)
+
+expandAddress :: String -> Int -> [Int]
+expandAddress mask address =
+  let
+    floatingBits = elemIndices 'X' $ reverse mask
+    -- Value of the mask when treating each X as 0 in order to overwrite all positions with 1
+    zeroedMask = sum $ map (2^) $ elemIndices '1' $ reverse mask
+    -- Value of the mask when treating each X as 0 in order to overwrite all positions with 0
+    unfloatMask = sum $ map (2^) $ elemIndices '1' $ replace "0" "1" $ reverse mask
+    baseAddress = (address .|. zeroedMask) .&. unfloatMask
+    -- For each possible floating bit combination, calculate the mask
+    floatingMasks = map (sum . map (2^)) $ subsequences floatingBits
+    in map (baseAddress .|.) floatingMasks
+    --in [baseAddress]
