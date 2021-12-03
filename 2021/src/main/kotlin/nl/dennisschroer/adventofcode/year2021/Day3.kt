be52eb2396eb2832537c5858c7256a47a487ec4a
dennis.schroer <dennis.schroer@topicus.nl>
2021-12-03 17:00:19 +0100

    ✨ Day 3 part 1

--- 2021/src/main/kotlin/nl/dennisschroer/adventofcode/year2021/Day3.kt
+++ 2021/src/main/kotlin/nl/dennisschroer/adventofcode/year2021/Day3.kt
@@ -2,19 +2,61 @@ package nl.dennisschroer.adventofcode.year2021
 
 class Day3 {
     fun part1(input: List<String>): Int {
-        return 0
+        return gammaRate(input).toInt(2) * epsilonRate(input).toInt(2)
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        return oxygenGeneratorRating(input).toInt(2) * co2ScrubberRating(input).toInt(2)
     }
 
     fun gammaRate(input: List<String>): String {
-        return ""
+        val length = input[0].length
+        return (0 until length).map { index -> mostOccuringChar(input.map { it[index] }) }.joinToString("")
     }
 
     fun epsilonRate(input: List<String>): String {
-        return ""
+        val length = input[0].length
+        return (0 until length).map { index -> leastOccuringChar(input.map { it[index] }) }.joinToString("")
+    }
+
+    fun oxygenGeneratorRating(input: List<String>): String {
+        var bitPosition = 0
+        var remainingInput = input
+        var mostOccuringChar: Char
+
+        while (remainingInput.size > 1) {
+            mostOccuringChar = mostOccuringChar(remainingInput.map { it[bitPosition] })
+            remainingInput = remainingInput.filter { it[bitPosition] == mostOccuringChar }
+            bitPosition++
+        }
+
+        return remainingInput.first()
+    }
+
+    fun co2ScrubberRating(input: List<String>): String {
+        var bitPosition = 0
+        var remainingInput = input
+        var mostOccuringChar: Char
+
+        while (remainingInput.size > 1) {
+            mostOccuringChar = leastOccuringChar(remainingInput.map { it[bitPosition] })
+            remainingInput = remainingInput.filter { it[bitPosition] == mostOccuringChar }
+            bitPosition++
+        }
+
+        return remainingInput.first()
+    }
+
+    fun mostOccuringChar(input: List<Char>): Char {
+        val counts = input.groupingBy { it }.eachCount()
+        // For part 2: if both occurences are equal pick '1'
+        return if (counts['0']!! > counts['1']!!) '0' else '1'
+    }
+
+    fun leastOccuringChar(input: List<Char>): Char {
+        val counts = input.groupingBy { it }.eachCount()
+        // For part 2: if both occurences are equal pick '0'
+        return if (counts['0']!! <= counts['1']!!) '0' else '1'
     }
 }
 
