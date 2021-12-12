85f046a3adacc0f7e5351a1947e95ffe57811bcc
dennis.schroer <dennis.schroer@topicus.nl>
2021-12-12 17:35:25 +0100

    ♻️Day 12 cleanup

--- 2021/src/main/kotlin/nl/dennisschroer/adventofcode/year2021/Day12.kt
+++ 2021/src/main/kotlin/nl/dennisschroer/adventofcode/year2021/Day12.kt
@@ -1,34 +1,24 @@
 package nl.dennisschroer.adventofcode.year2021
 
 class Day12 {
-    data class Graph(val nodes: Set<Node>, val edges: List<Edge>)
+    data class Graph(val nodes: Set<Node>)
     data class Node(val id: String, val edges: MutableList<Edge>) {
         override fun equals(other: Any?): Boolean = if (other is Node) this.id == other.id else false
         override fun hashCode(): Int = id.hashCode()
         override fun toString(): String = id
     }
-
     data class Edge(val start: Node, val end: Node)
     data class Path(val nodes: List<Node>)
 
     fun part1(input: List<String>): Int {
         val graph = inputToGraph(input)
-
-        val start = graph.nodes.first { it.id == "start" }
-        val end = graph.nodes.first { it.id == "end" }
-        val visited = listOf<Node>()
-
-        val routes = findAllPaths(start, end, visited)
-        routes.forEach { println(it) }
-        return routes.size
+        return findAllPaths(graph.nodes.first { it.id == "start" }, graph.nodes.first { it.id == "end" }, listOf()).size
     }
 
     /**
      * Find all possible paths from start to end by recursively walking each edge from the start node.
      */
-    fun findAllPaths(start: Node, end: Node, visited: List<Node>): Set<Path> {
-        println("Finding all paths from $start to $end; visited: $visited")
-
+    private fun findAllPaths(start: Node, end: Node, visited: List<Node>): Set<Path> {
         return start.edges.filter { !visited.contains(it.end) }.map { edge ->
             if (edge.start == end) {
                 // Base case
@@ -41,10 +31,15 @@ class Day12 {
         }.flatten().toSet()
     }
 
-    fun findAllPathsWhileVisitingOneSmallCaveTwice(start: Node, end: Node, visited: List<Node>): Set<Path> {
-        println("Finding all paths from $start to $end; visited: $visited")
+    fun part2(input: List<String>): Int {
+        val graph = inputToGraph(input)
+        return findAllPathsWhileVisitingOneSmallCaveTwice(graph.nodes.first { it.id == "start" }, graph.nodes.first { it.id == "end" }, listOf()).size
+    }
 
-
+    /**
+     * Find all possible paths from start to end where one of the small nodes can be visited twice.
+     */
+    private fun findAllPathsWhileVisitingOneSmallCaveTwice(start: Node, end: Node, visited: List<Node>): Set<Path> {
         return start.edges.map { edge ->
             val newVisited = if (start.id.first().isLowerCase()) visited + start else visited
 
@@ -62,47 +57,17 @@ class Day12 {
                 findAllPathsWhileVisitingOneSmallCaveTwice(edge.end, end, newVisited).map { Path(listOf(start) + it.nodes) }
             }
         }.flatten().toSet()
-
-
-        val defaultRoutes = findAllPathsWhileVisitingOneSmallCaveTwice(start, end, visited)
-
-        // When in here, we can also still opt to visit one small cave for the second time
-        val backingRoutes = start.edges.filter { visited.contains(it.end) }.map { edge ->
-            val newVisited = if (start.id.first().isLowerCase()) visited + start else visited
-            findAllPaths(edge.end, end, newVisited)
-        }.flatten().toSet()
-
-        return defaultRoutes + backingRoutes
     }
 
-    fun part2(input: List<String>): Int {
-        val graph = inputToGraph(input)
-
-        val start = graph.nodes.first { it.id == "start" }
-        val end = graph.nodes.first { it.id == "end" }
-        val visited = listOf<Node>()
-
-        val routes = findAllPathsWhileVisitingOneSmallCaveTwice(start, end, visited)
-        routes.forEach { println(it) }
-        return routes.size
-    }
-
-    fun inputToGraph(input: List<String>): Graph {
-        val edges = mutableListOf<Edge>()
-        val nodes = mutableSetOf<Node>()
-
-        input.forEach { line ->
+    private fun inputToGraph(input: List<String>): Graph {
+        return Graph(input.fold(setOf()) { nodes, line ->
             val nodeA = line.substringBefore('-').let { id -> nodes.find { it.id == id } ?: Node(id, mutableListOf()) }
             val nodeB = line.substringAfter('-').let { id -> nodes.find { it.id == id } ?: Node(id, mutableListOf()) }
-            val edge = Edge(nodeA, nodeB)
-            nodes.add(nodeA)
-            nodes.add(nodeB)
             nodeA.edges.add(Edge(nodeA, nodeB))
             nodeB.edges.add(Edge(nodeB, nodeA))
-            edges.add(edge)
-        }
 
-        return Graph(nodes, edges)
+            nodes + nodeA + nodeB
+        })
     }
 }
 
