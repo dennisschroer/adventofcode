bbda9f6211afb641ecd7acea43f2c51fcc22af75
dennis schroer <dev@dennisschroer.nl>
2021-12-21 18:36:23 +0100

    ✨ Day 18 WIP

--- 2021/src/main/kotlin/nl/dennisschroer/adventofcode/year2021/Day18.kt
+++ 2021/src/main/kotlin/nl/dennisschroer/adventofcode/year2021/Day18.kt
@@ -1,77 +1,143 @@
 package nl.dennisschroer.adventofcode.year2021
 
+import kotlin.random.Random
+
 class Day18 {
     abstract class Node(var parent: Node?) {
         fun depth(): Int = if (parent == null) 1 else 1 + (parent as Node).depth()
+        abstract fun isLike(other : Node) : Boolean
     }
 
-    data class PairNode(var pair: Pair<Node, Node>) : Node(null)
-    data class ValueNode(var value: Int) : Node(null)
+    class PairNode(var pair: Pair<Node, Node>) : Node(null) {
+        private val id = Random.nextInt()
+        init {
+            pair.first.parent = this
+            pair.second.parent = this
+        }
 
-    data class Value(val value: Int, val depth: Int)
+        override fun isLike(other: Node) = other is PairNode && this.pair.first.isLike(other.pair.first) && this.pair.second.isLike(other.pair.second)
+        override fun equals(other: Any?): Boolean = other is PairNode && this.id == other.id
+        override fun toString(): String = "[${pair.first},${pair.second}]"
+    }
+
+    data class ValueNode(var value: Int) : Node(null) {
+        override fun isLike(other: Node) = other is ValueNode && this.value == other.value
+        override fun toString(): String = "$value"
+    }
 
     fun part1(input: List<String>): Int {
-        return input.map { parseToFish(it) }.reduce(this::addAndReduce).let { magnitude(it) }
+        return addAndReduceAll(input.map { parseToFish(it) }).let { magnitude(it) }
+    }
+
+    fun addAndReduceAll(fishes: List<Node>): Node {
+        return fishes.reduce(this::addAndReduce)
     }
 
     fun part2(input: List<String>): Int {
         return -1
     }
 
-    private fun addAndReduce(fish1: List<Value>, fish2: List<Value>): List<Value> {
-        return reduce(flattenFish(PairNode(fish1 to fish2)))
+    fun addAndReduce(fish1: Node, fish2: Node): Node {
+        println("  $fish1\n+ $fish2")
+        val parent = PairNode(fish1 to fish2)
+        fish1.parent = parent
+        fish2.parent = parent
+        val result = reduce(parent)
+        println("= $result")
+        return result
     }
 
-    private fun reduce(fish: List<Value>): List<Value> {
-        return explodeOrSplit(fish)
+    fun reduce(fish: Node): Node {
+        repeat(1000) { explodeOrSplit(fish) }
+        println()
+        return fish
     }
 
-    private fun explodeOrSplit(fish: Node): Node {
-        // Lets walk the tree
-        var currentNode: Node? = fish
-        while (currentNode is PairNode) {
-            currentNode = currentNode.pair.first
-        }
-
+    fun explodeOrSplit(fish: Node): Node {
         var done = false
-        while (!done && currentNode != null) {
-            if (currentNode.depth() >= 4) {
-                explode(currentNode as PairNode)
+
+        // Lets walk the tree
+        walk(fish, { pairNode: PairNode ->
+            if (!done && pairNode.depth() > 4) {
+                explode(pairNode)
                 done = true
             }
+        }, { })
 
-            currentNode = right(currentNode)
+        walk(fish, { }, { valueNode: ValueNode ->
+            if (!done && valueNode.value > 9) {
+                split(valueNode)
+                done = true
+            }
+        })
+
+        if (done) {
+            println("   => $fish")
+        } else {
+            print("*")
         }
 
+        return fish
+    }
+
+    fun walk(fish: Node, visitPair: (pairNode: PairNode) -> Unit, visitValue: (valueNode: ValueNode) -> Unit) {
+        if (fish is PairNode) {
+            walk(fish.pair.first, visitPair, visitValue)
+            visitPair(fish)
+            walk(fish.pair.second, visitPair, visitValue)
+        } else {
+            visitValue(fish as ValueNode)
+        }
     }
 
     private fun explode(node: PairNode) {
+        println("Exploding $node")
         left(node)?.let {
+            println("  Left: $it")
             it.value = it.value + (node.pair.first as ValueNode).value
         }
         right(node)?.let {
+            println("  Right: $it")
             it.value = it.value + (node.pair.second as ValueNode).value
         }
 
         val parent = node.parent as PairNode
+        val newNode = ValueNode(0)
+        newNode.parent = parent
         if (node == parent.pair.first) {
-            parent.pair = ValueNode(0) to parent.pair.second
+            parent.pair = newNode to parent.pair.second
         }
         if (node == parent.pair.second) {
-            parent.pair = parent.pair.first to ValueNode(0)
+            parent.pair = parent.pair.first to newNode
+        }
+    }
+
+    private fun split(node: ValueNode) {
+        println("Splitting $node, parent: ${node.parent}")
+        val newNode = PairNode(ValueNode(node.value / 2) to ValueNode(node.value - (node.value / 2)))
+        newNode.pair.first.parent = newNode
+        newNode.pair.second.parent = newNode
+        val parent = node.parent as PairNode
+        newNode.parent = parent
+        if (node == parent.pair.first) {
+            parent.pair = newNode to parent.pair.second
+        }
+        if (node == parent.pair.second) {
+            parent.pair = parent.pair.first to newNode
         }
     }
 
     private fun left(fish: Node): ValueNode? {
-        var currentNode = fish
+        var currentNode: Node? = fish
 
         // Traverse up
-        while (currentNode.parent != null && currentNode.parent is PairNode && currentNode == (currentNode.parent as PairNode).pair.second) {
+        while (currentNode!!.parent != null && currentNode.parent is PairNode && currentNode == (currentNode.parent as PairNode).pair.first) {
             currentNode = currentNode.parent as PairNode
         }
+        currentNode = currentNode.parent
 
-        // Exception: if this is the leftmost node currentNode is a value
-        if (currentNode is ValueNode) return null
+        // Exception: if this is the leftmost node return null
+        if (currentNode == null) return null
 
         // Go left
         currentNode = (currentNode as PairNode).pair.first
@@ -85,15 +151,16 @@ class Day18 {
     }
 
     private fun right(fish: Node): ValueNode? {
-        var currentNode = fish
+        var currentNode: Node? = fish
 
         // Traverse up
-        while (currentNode.parent != null && currentNode.parent is PairNode && currentNode == (currentNode.parent as PairNode).pair.first) {
+        while (currentNode!!.parent != null && currentNode.parent is PairNode && currentNode == (currentNode.parent as PairNode).pair.second) {
             currentNode = currentNode.parent as PairNode
         }
+        currentNode = currentNode.parent
 
-        // Exception: if this is the rightmost node currentNode is a value
-        if (currentNode is ValueNode) return null
+        // Exception: if this is the rightmost node return null
+        if (currentNode == null) return null
 
         // Go right
         currentNode = (currentNode as PairNode).pair.second
@@ -107,21 +174,18 @@ class Day18 {
     }
 
 
-    private fun magnitude(fish: List<Pair<Int, Int>>): Int {
-        return -1
-    }
-
-    fun flattenFish(fish: Node, depth: Int = 1): List<Value> {
-        if (fish is PairNode) {
-            return flattenFish(fish.pair.first, depth + 1) + flattenFish(fish.pair.second, depth + 1)
+    fun magnitude(fish: Node): Int {
+        return if (fish is PairNode) {
+            3 * magnitude(fish.pair.first) + 2 * magnitude(fish.pair.second)
+        } else {
+            (fish as ValueNode).value
         }
-        return listOf(Value((fish as ValueNode).value, depth))
     }
 
-    fun parseToFish(line: String): List<Value> {
+    fun parseToFish(line: String): Node {
         val (node, result) = parseToNode(line)
         assert(result.isEmpty())
-        return flattenFish(node)
+        return node
     }
 
     fun parseToNode(line: String): Pair<Node, String> {
@@ -133,7 +197,8 @@ class Day18 {
             subNode2.parent = parent
             parent to remaining2.drop(1)
         } else {
-            ValueNode(line[0].digitToInt()) to line.drop(1)
+
+            ValueNode(line.takeWhile { it.isDigit() }.toInt()) to line.dropWhile { it.isDigit() }
         }
     }
 }
--- 2021/src/test/kotlin/nl/dennisschroer/adventofcode/year2021/Day18Test.kt
+++ 2021/src/test/kotlin/nl/dennisschroer/adventofcode/year2021/Day18Test.kt
@@ -1,9 +1,94 @@
 package nl.dennisschroer.adventofcode.year2021
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 internal class Day18Test {
+    @Test
+    fun testExplode() {
+        val d = Day18()
+        assertLike(d.parseToFish("[[[[0,9],2],3],4]"), d.explodeOrSplit(d.parseToFish("[[[[[9,8],1],2],3],4]")))
+        assertLike(d.parseToFish("[7,[6,[5,[7,0]]]]"), d.explodeOrSplit(d.parseToFish("[7,[6,[5,[4,[3,2]]]]]")))
+        assertLike(d.parseToFish("[[6,[5,[7,0]]],3]"), d.explodeOrSplit(d.parseToFish("[[6,[5,[4,[3,2]]]],1]")))
+        assertLike(d.parseToFish("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"), d.explodeOrSplit(d.parseToFish("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]")))
+        assertLike(d.parseToFish("[[3,[2,[8,0]]],[9,[5,[7,0]]]]"), d.explodeOrSplit(d.parseToFish("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")))
+        assertLike(
+            d.parseToFish("[[[[0,[13,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]],[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]]"),
+            d.explodeOrSplit(d.parseToFish("[[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]],[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]]"))
+        )
+    }
+
+    private fun assertLike(fishExpected: Day18.Node, fishActual: Day18.Node) {
+        assertTrue(fishExpected.isLike(fishActual), "Fishes are not the same\nExpected: $fishExpected\nActual:   $fishActual")
+    }
+
+    @Test
+    fun testReduce() {
+        val d = Day18()
+        assertLike(d.parseToFish("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"), d.reduce(d.parseToFish("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]")))
+    }
+
+    @Test
+    fun testMagnitude() {
+        val d = Day18()
+        assertEquals(29, d.magnitude(d.parseToFish("[9,1]")))
+        assertEquals(21, d.magnitude(d.parseToFish("[1,9]")))
+        assertEquals(143, d.magnitude(d.parseToFish("[[1,2],[[3,4],5]]")))
+        assertEquals(3488, d.magnitude(d.parseToFish("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]")))
+    }
+
+    @Test
+    fun testAddAndReduce() {
+        val d = Day18()
+        assertEquals(
+            d.parseToFish("[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]"),
+            d.addAndReduce(
+                d.parseToFish("[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]"),
+                d.parseToFish("[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]")
+            )
+        )
+        
+        assertLike(
+            d.parseToFish("[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]"),
+            d.addAndReduce(
+                d.parseToFish("[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]"),
+                d.parseToFish("[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]")
+            )
+        )
+    }
+
+    @Test
+    fun testAddAndReduceAll() {
+        val d = Day18()
+        assertLike(
+            d.parseToFish("[[[[1,1],[2,2]],[3,3]],[4,4]]"),
+            d.addAndReduceAll(listOf("[1,1]", "[2,2]", "[3,3]", "[4,4]").map { d.parseToFish(it) })
+        )
+        assertLike(
+            d.parseToFish("[[[[3,0],[5,3]],[4,4]],[5,5]]"),
+            d.addAndReduceAll(listOf("[1,1]", "[2,2]", "[3,3]", "[4,4]", "[5,5]").map { d.parseToFish(it) })
+        )
+        assertLike(
+            d.parseToFish("[[[[5,0],[7,4]],[5,5]],[6,6]]"),
+            d.addAndReduceAll(listOf("[1,1]", "[2,2]", "[3,3]", "[4,4]", "[5,5]", "[6,6]").map { d.parseToFish(it) })
+        )
+        assertLike(
+            d.parseToFish("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"),
+            d.addAndReduceAll(listOf(
+                "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+                "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]",
+                "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]",
+                "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]",
+                "[7,[5,[[3,8],[1,4]]]]",
+                "[[2,[2,2]],[8,[8,1]]]",
+                "[2,9]",
+                "[1,[[[9,3],9],[[9,0],[0,7]]]]",
+                "[[[5,[7,4]],7],1]",
+                "[[[[4,2],2],6],[8,7]]"
+            ).map { d.parseToFish(it) })
+        )
+    }
 
     @Test
     fun testPart1() {
