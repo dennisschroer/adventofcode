039789cb7d437c3196c9eab0e1f721f679e0e7fc
dennis schroer <dev@dennisschroer.nl>
2021-12-31 12:47:43 +0100

    ✨ Day 24 Part 1

--- 2021/src/main/kotlin/nl/dennisschroer/adventofcode/year2021/Day24.kt
+++ 2021/src/main/kotlin/nl/dennisschroer/adventofcode/year2021/Day24.kt
@@ -21,9 +21,31 @@ class Day24 {
     data class Memory(val w: Int = 0, val x: Int = 0, val y: Int = 0, val z: Int = 0)
     data class State(val memory: Map<Char, Int> = mutableMapOf('w' to 0, 'x' to 0, 'y' to 0, 'z' to 0), val input: List<Int> = listOf())
 
+    // Observations:
+    // - 1: a is negative iff c == 26
+    // - 2: a > 10 if c == 1, thus x != w
+    // - 3: Calculations can be seen as if in base 26
+    //
+    // x = z % 26 + a
+    // z = z / c
+    // if (x != input) {
+    //    z = (26 * z) + input + b
+    // }
+    // return z
+    //              0   1   2   3   4   5    6   7   8   9  10  11  12  13
     val a = listOf(11, 14, 10, 14, -8, 14, -11, 10, -6, -9, 12, -5, -4, -9)
-    val b = listOf(7, 8, 16, 8, 3, 12, 1, 8, 8, 14, 4, 14, 15, 6)
-    val c = listOf(1, 1, 1, 1, 26, 1, 26, 1, 26, 26, 1, 26, 26, 26)
+    val b = listOf(7,  8,  16,  8,  3, 12,   1,  8,  8, 14,  4, 14, 15,  6)
+    val c = listOf(1,  1,   1,  1, 26,  1,  26,  1, 26, 26,  1, 26, 26, 26)
+
+    // Conclusions:
+    // w4 == w3                    w3=9 w4=9
+    // w6 == w5 + 1 mod 26         w5=8 w6=9
+    // w8 == w7 + 2 mod 26         w7=7 w8=9
+    // w9 == w2 + 7 mod 26         w2=2 w9=9
+    // w11 == w10 - 1 mod 26       w10=9 w11=8
+    // w12 == w1 + 4 mod 26        w1=5 w12=9
+    // w13 == w0 - 2 mod 26        w0=9 w13=7
+    // => largest = 95299897999897
 
     fun part1(input: List<String>): Long {
         val commands = parseCommands(input)
@@ -53,22 +75,18 @@ class Day24 {
             println(index)
 
             val possibleZs = possibleOutcomes
-            possibleOutcomes = listOf(9, 8, 7, 6, 5, 4, 3, 2, 1).flatMap { input ->
-                digits[index] = input
-                possibleZs.map { z ->
-                    val outcome = custom(input, z, a[index], b[index], c[index])
-                    if (outcome == 0) {
-                        println("OUTPUT 0 at index $index for input $input and z $z, digits = ${digits.map { it.value }.joinToString("")}")
-                    }
-                    outcome
-                }.also { println() }
-            }.toSortedSet()
-
-
-            //println(possibleOutcomes)
-            //println(commands[5])
-            //println(commands[15])
-            //println(commands[4])
+            if (a[index] < 10) { // Optimization from observation 2
+                possibleOutcomes = listOf(9, 8, 7, 6, 5, 4, 3, 2, 1).flatMap { input ->
+                    digits[index] = input
+                    possibleZs.map { z ->
+                        val outcome = custom(input, z, a[index], b[index], c[index])
+                        if (outcome == 0) {
+                            println("OUTPUT 0 at index $index for input $input and z $z, digits = ${digits.map { it.value }.joinToString("")}")
+                        }
+                        outcome
+                    }.also { println() }
+                }.toSortedSet()
+            }
         }
 
         println(possibleOutcomes.contains(0))
