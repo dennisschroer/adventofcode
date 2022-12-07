cf26b8ec2257c1b71e46d3f7cc17b83c6e196d51
Dennis Schroer <dennis.schroer@sqills.com>
2022-12-07 18:11:10 +0100

    Update utils

--- 2022/src/main/kotlin/nl/dennisschroer/adventofcode/year2022/utils.kt
+++ 2022/src/main/kotlin/nl/dennisschroer/adventofcode/year2022/utils.kt
@@ -1,24 +1,24 @@
 package nl.dennisschroer.adventofcode.year2022
 
 // A class is required to use the class loader
-sealed class Loader{}
+sealed class Loader {}
 
 fun readFile(filename: String): String {
-    return Loader::class.java.getResource("/$filename.txt")!!.readText().trim()
+    return Loader::class.java.getResource("/$filename.txt")!!.readText().trimEnd()
 }
 
 fun readLines(filename: String): List<String> {
     return readFile(filename).split("\n")
 }
 
-fun readBlocks(filename:String):List<List<String>> {
-    return readFile(filename).split("\n\n").map {it.split("\n")}
+fun readBlocks(filename: String): List<List<String>> {
+    return readFile(filename).split("\n\n").map { it.split("\n") }
 }
 
 fun readNumbers(filename: String): List<Int> {
     return readLines(filename).map { it.toInt() }
 }
 
-fun IntRange.fullyContains(other: IntRange): Boolean = first <=other.first && last >= other.last
+fun IntRange.fullyContains(other: IntRange): Boolean = first <= other.first && last >= other.last
 fun IntRange.overlaps(other: IntRange): Boolean = first <= other.last && last >= other.first
 val IntRange.length: Int get() = if (last >= first) last - first + 1 else 0
